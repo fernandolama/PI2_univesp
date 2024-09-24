744b7eda032f72ac49f6107a6c360c1d2a76e69b
Fernando <dellama.f@gmail.com>
2024-09-24 17:42:56 -0300

    add models

--- app_foto/models.py
+++ app_foto/models.py
@@ -1,3 +1,4 @@
+import enum
 from django.db import models
 
 # Create your models here.
@@ -9,11 +10,18 @@ class BaseModel(models.Model):
 class Client(BaseModel):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
+    birthday = models.DateField()
+
+    def __str__(self):
+        return f'{self.name}'
     
 class Cellphone(BaseModel):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='cellphones')
     area_code = models.IntegerField(max_length=2)
     number = models.IntegerField(max_length=9)
+
+    def __str__(self):
+        return f'{self.client.name} - ({self.area_code}) {self.number}'
     
 class Address(BaseModel):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='addresses')
@@ -23,3 +31,31 @@ class Address(BaseModel):
     zipcode = models.IntegerField(max_length=8)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f'{self.client.name} - {self.street}, {self.number} - {self.complement}\n{self.zipcode} {self.city}-{self.state}'
+
+class ServiceType(enum.Enum):
+    CHA_REVELACAO = "Chá revelação"
+    CHA_BEBE = "Chá de bebê"
+    ACOMPANHAMENTO_12_MESES = "Acompanhamento_12 meses"
+    ANIVERSARIO_1_ANO = "Aniversário_1 ano"
+    ANIVERSARIO_OUTROS = "Aniversário_outros"
+    FESTA_DEBUTANTE = "Festa de debutante"
+    PRE_WEDDING = "Pré-Wedding"
+    CASAMENTO = "Casamento"
+    OUTROS = "Outros"
+
+class Service(BaseModel):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='services')
+    service_type = models.CharField(
+        max_length=50,
+        choices=[(tag.name, tag.value) for tag in ServiceType]
+    )
+    description = models.TextField(blank=True, null=True)
+    date = models.DateField()
+    hour = models.TimeField()
+    local = models.ForeignKey(Address, on_delete=models.CASCADE, related_name='services')
+
+    def __str__(self):
+        return f'{self.client.name} - {self.get_service_type_display()}'
